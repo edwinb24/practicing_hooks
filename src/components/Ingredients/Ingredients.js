885bb12edb955e445784b5b48390b8dea6352f85
edwinb24 <edwin_b24@hotmail.com>
2020-07-14 14:27:54 -0500

    Practicing Hooks useState and props

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList'
 
-function Ingredients() {
+function Ingredients(props) {
+  let [userIngredients, setUserIngredients] = useState([])
+
+  const addIngredientHandler = inputIngredient => {
+    setUserIngredients(prevIngredients =>
+      [
+        ...prevIngredients,
+        { id: Math.random().toString(), ...inputIngredient }
+      ]
+    )
+  }
+
+  const removeIngredientHandler = id => {
+        setUserIngredients(prevIngredients => 
+          prevIngredients.filter((ingredient) => ingredient.id !== id)
+        )
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
       </section>
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeIngredientHandler}
+      />
     </div>
   );
 }
