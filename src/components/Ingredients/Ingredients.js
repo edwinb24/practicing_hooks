8f07e70c71bc75038dfc30e030bd203be376f011
edwinb24 <edwin_b24@hotmail.com>
2020-07-16 10:51:27 -0500

    Adding search functionality using useEffect, useRef, and useCallBack

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,27 +7,68 @@ import IngredientList from './IngredientList'
 function Ingredients(props) {
   let [userIngredients, setUserIngredients] = useState([])
 
+  // useEffect(() => {
+  //   fetch('https://react-hooks-update-9c3e3.firebaseio.com/ingredients.json')
+  //     .then(resp => resp.json())
+  //     .then(responseData => {
+  //       const loadedIngredients = []
+  //       console.log(responseData)
+  //       for(const key in responseData) {
+
+  //         loadedIngredients.push(
+  //           {id:key, 
+  //             title: responseData[key].title, 
+  //             amount: responseData[key].amount
+  //           }
+  //         )
+  //       }
+  //       setUserIngredients(loadedIngredients)
+  //     })
+  // }, [])
+
   const addIngredientHandler = inputIngredient => {
-    setUserIngredients(prevIngredients =>
-      [
-        ...prevIngredients,
-        { id: Math.random().toString(), ...inputIngredient }
-      ]
+    fetch(
+      'https://react-hooks-update-9c3e3.firebaseio.com/ingredients.json',
+      {
+        method: "POST",
+        body: JSON.stringify(inputIngredient),
+        headers: { 'Content-Type': 'application/json' }
+      }
     )
+      .then(resp => resp.json())
+      .then(responseData => {
+        setUserIngredients(prevIngredients =>
+          [
+            ...prevIngredients,
+            { id: responseData.name, ...inputIngredient }
+          ]
+        )
+      })
   }
 
   const removeIngredientHandler = id => {
-        setUserIngredients(prevIngredients => 
-          prevIngredients.filter((ingredient) => ingredient.id !== id)
+        fetch(`https://react-hooks-update-9c3e3.firebaseio.com/ingredients/${id}.json`, 
+          {
+            method: "DELETE"
+          }
         )
+      .then(() => {
+        setUserIngredients(prevIngredients =>
+          prevIngredients.filter((ingredient) => ingredient.id !== id)
+        )  
+      })  
   }
+
+  const filterIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients)
+  }, [setUserIngredients])
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filterIngredientsHandler}/>
         {/* Need to add list here! */}
       </section>
       <IngredientList
